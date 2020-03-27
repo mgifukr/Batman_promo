a76468aab6486931a25a26ca6ebbbe7c4a4fe402
Mike_Gerula <gerula.mike@gmail.com>
2020-03-27 20:55:59 +0200

    new edits and files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+   let tabsItem = $('.tabs__item');
+   console.log(tabsItem);
+   tabsItem.on('click', function (event) {
+      event.preventDefault();
+      let activeContent = $(this).attr('href');
+      $('.visible').toggleClass('visible');
+      $(activeContent).toggleClass('visible');
+      $('.tabs__item-active').toggleClass('tabs__item-active');
+      $(this).toggleClass('tabs__item-active');
+
+
+   })
+});
